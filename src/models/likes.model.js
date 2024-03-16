b0a5d5b8b8dc0f100641f06c46d2e5c7e3e84c74
atif shaikh <shaikhatif456a@gmail.com>
2024-03-16 13:16:43 +0530

    add models

---
+++ src/models/likes.model.js
@@ -0,0 +1,28 @@
+import mongoose ,{Schema, mongo}from "mongoose"
+
+const likeSchema = new Schema(
+    {
+     
+     
+     comment:{
+        type: mongoose.Types.ObjectId,
+        ref:"Comment"
+     },
+     video:{
+        type:mongoose.Types.ObjectId,
+        ref :"Video"
+     },
+     likedBy:{
+        type: mongoose.Types.ObjectId,
+        ref:"User"
+     },
+     tweet:{
+        type: mongoose.Types.ObjectId,
+        ref:"Tweet"
+     }
+   },{
+    timestamps:true
+   }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
